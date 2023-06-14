ef939617811c6b3ba2f807ccf0b07b33235d9998
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-06-15 00:37:22 +0330

    Add Some Options to GeneralSettings

--- TvTime/Views/Settings/GeneralSettingPage.xaml.cs
+++ TvTime/Views/Settings/GeneralSettingPage.xaml.cs
@@ -16,8 +16,15 @@ public sealed partial class GeneralSettingPage : Page
     {
         var iconPack = Settings.IconPack;
         var descType = Settings.DescriptionType;
+
+        var headerStyle = Settings.SettingsCardHeaderStyle;
+        var descriptionStyle = Settings.SettingsCardDescriptionStyle;
+
         cmbIconPack.SelectedItem = cmbIconPack.Items.FirstOrDefault(x => ((ComboBoxItem) x).Tag.ToString() == iconPack.ToString());
         cmbDescriptionType.SelectedItem = cmbDescriptionType.Items.FirstOrDefault(x => ((ComboBoxItem) x).Tag.ToString() == descType.ToString());
+
+        cmbHeaderStyle.SelectedItem = cmbHeaderStyle.Items.FirstOrDefault(x => (string)x == headerStyle);
+        cmbDescriptionStyle.SelectedItem = cmbDescriptionStyle.Items.FirstOrDefault(x => (string)x == descriptionStyle);
     }
 
     protected override void OnNavigatedTo(NavigationEventArgs e)

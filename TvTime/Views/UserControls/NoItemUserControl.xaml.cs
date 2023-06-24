998da118ad59f91ba378f4a2a69801a529b1c833
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-06-24 18:48:40 +0330

    Add No Item Found UI

---
+++ TvTime/Views/UserControls/NoItemUserControl.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace TvTime.Views;
+public sealed partial class NoItemUserControl : UserControl
+{
+    public int Count
+    {
+        get { return (int) GetValue(CountProperty); }
+        set { SetValue(CountProperty, value); }
+    }
+
+    public static readonly DependencyProperty CountProperty =
+        DependencyProperty.Register("Count", typeof(int), typeof(NoItemUserControl), new PropertyMetadata(0));
+
+
+    public NoItemUserControl()
+    {
+        this.InitializeComponent();
+    }
+}

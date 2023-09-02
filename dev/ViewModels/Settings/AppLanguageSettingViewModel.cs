a0f46a875edc4ff271c47cf37a0a5ab9787da7c3
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-09-02 14:22:40 +0330

    Update to WASDK 1.4

--- dev/ViewModels/Settings/AppLanguageSettingViewModel.cs
+++ dev/ViewModels/Settings/AppLanguageSettingViewModel.cs
@@ -16,14 +16,7 @@ public partial class AppLanguageSettingViewModel : ObservableRecipient
                 Settings.TvTimeLanguage = selectedItem;
                 App.Current.Localizer.SetLanguage(selectedItem.LanguageCode);
                 MainPage.Instance.SetFlowDirection();
-                if (ApplicationHelper.IsPackaged)
-                {
-                    Microsoft.Windows.AppLifecycle.AppInstance.Restart("");
-                }
-                else
-                {
-                    ApplicationHelper.Restart();
-                }
+                Microsoft.Windows.AppLifecycle.AppInstance.Restart("");
             }
         }
     }
